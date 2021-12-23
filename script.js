cf6d8a3eb8583c1eb248748696ecc573b03bbef4
Fernisk <miyaresf90@gmail.com>
2021-12-23 13:15:16 -0500

    modified, shows the winner each round

--- script.js
+++ script.js
@@ -1,64 +1,65 @@
 // ROCK PAPER SCISSORS
-// NO GUI AT THE MOMENT(SUBJECT TO CHANGE)
 
 //create array with desired values
 
-const hand = [
-    'rock', // hand[0]
-    'paper',  // hand[1]
-    'scissors' // hand[2]
-];
+const hand = {
+
+    rock: "rock", // hand[0]
+    paper: "paper",  // hand[1]
+    scissors: "scissors" // hand[2]
+};
 
 //keep score
  let playerScore = 0;
  let computerScore = 0;
 
-
 // computerPlay() will return randomly either rock, paper or scissors.
 function computerPlay(){
-    let result;
-    result = hand[Math.floor(Math.random() * hand.length)];
+    const keys = Object.keys(hand);
+    const result = keys[Math.floor(Math.random() * keys.length)];
     return result
 }
-// playerPlay() will return the input from the player
-// function playerPlay(){
-//     let input;
-//     input = prompt("What is your choice"); 
-//     return input;
-// }
-
+//computerSelection
 const computerSelection = computerPlay();
- // game() should (for now) play a 5 round game that keeps score and reports a winner or loser at the end.
- function game() {
-     // while i is less than 5(5 rounds)
-     let i = 0;
-     
-    while(i < 5){
-    //     //prompt player for input
-        const playerSelection = playerPlay();
-        console.log(playRound(playerSelection, computerSelection));
-    //     //playRound()
-    //     //return winner each time
-        i++;
-    }
-    if (playerScore > computerScore){
-        console.log("YOU WIN");
-    } else if (computerScore > playerScore){
-        console.log("COMPUTER WINS");
-    } else {
-        console.log("Draw")
-    }
- }
+// img buttons
+const buttons = document.querySelectorAll("input");
+const article = document.querySelector("article");
+const p = document.querySelector("p");
 
+const newPara = document.createElement("p"); 
+newPara.setAttribute('id', "added");
+newPara.style.marginTop = "20px";
+newPara.style.fontSize = "50px";
+article.appendChild(newPara);
+
+let playerSelection;
+let countButtonClick = 0;
+let choice = "";
+        
+//each time pressed, delete the previous paragraph
+    buttons.forEach(button => {
+        button.addEventListener("click", (e) => {
+         countButtonClick++;
+         playerSelection = e.target.value;
+         choice = playRound(playerSelection, computerSelection);
+         getTextContent();
+        });
+    });    
+   // will update the output paragraph
+function getTextContent(){
+    let result = "";
+    for (let i = 0; i < choice.length; i++){
+        result += choice[i];
+    }
+    document.getElementById("added").textContent = result;
+}
+ 
 // playRound() will play one round and return either if player 
 // wins or loses.
 function playRound(playerSelection, computerSelection) {
-    // error check
-    if (typeof playerSelection !== 'string'){
-        return  "input a string"
-    }
-    // case-insensitive (convert to lowercase)
-    const player = playerSelection.toLowerCase();
+    
+  
+    let player = playerSelection;
     const computer = computerSelection;
     //if they are the same = return it's a draw
     //if rock against scissors || paper goes against rock || scissors against paper
@@ -66,18 +67,41 @@ function playRound(playerSelection, computerSelection) {
     if (player === computer){
         return "It's a draw";
     }
-    else if (player === hand[0] && computer === hand[2] || player === hand[1] && computer === hand[0] || player === hand[2] && computer === hand[1]){
+    else if (player === hand.rock && computer === hand.scissors || player === hand.paper && computer === hand.rock || player === hand.scissors && computer === hand.paper){
         playerScore++;
        return `You Win! ` + player + ' beats ' + computer; 
     }
     else {
 
         computerScore++;
-        return `You Lose! ` + player + ' beats ' + computer;
+        return `You Lose! ` + computer + ' beats ' + player;
     }
     
        
 }
+//  // game() should (for now) play a 5 round game that keeps score and reports a winner or loser at the end.
+//  function game() {
+//      // while i is less than 5(5 rounds)
+//      let i = 0;
+     
+//     // while(i < 5){
+//     // //     //prompt player for input
+//     //     const playerSelection = playerPlay();
+//     //     console.log(playRound(playerSelection, computerSelection));
+//     // //     //playRound()
+//     // //     //return winner each time
+//     //     i++;
+//     // }
+//     if (playerScore > computerScore){
+//         console.log("YOU WIN");
+//     } else if (computerScore > playerScore){
+//         console.log("COMPUTER WINS");
+//     } else {
+//         console.log("Draw")
+//     }
+//  }
 
 
- game();
+
+
+// //  game();
